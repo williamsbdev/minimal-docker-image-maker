767798ec5eb530b3b2660852d261d3f2961aba3b
Brandon Williams <williamsbdev@gmail.com>
2017-02-18 20:50:12 -0600

    [REFACTOR] clean up code

--- create_minimal_image.py
+++ create_minimal_image.py
@@ -2,54 +2,45 @@ import subprocess
 import sys
 
 
-def ldd_of_shared_object(shared_objects, executable):
-    std_out = run_bash_command(["ldd", str(executable).strip()])
-    return std_out_to_shared_objects(shared_objects, std_out)
-
-
-def std_out_to_shared_objects(shared_objects, std_out):
-    for std_out_line in convert_std_out_to_list(std_out):
-        find_dependencies(shared_objects, std_out_line)
+def main(directory):
+    find_command = ['find', directory, '-type', 'f', '-perm', '/a+x', '-exec', 'ldd', '{}', ';']
+    jvm_find_std_out = _run_bash_command(find_command)
+    shared_objects = _std_out_to_shared_objects({}, jvm_find_std_out)
     return shared_objects
 
 
-def find_dependencies(shared_objects, std_out_line):
-    line_list = std_out_line.split(" ")
-    shared_object_name = line_list[0].strip()
-    if shared_object_name not in shared_objects.keys():
-        if len(line_list) > 3 and line_list[2] not in ('', 'not'):
-            shared_objects[shared_object_name] = line_list[2]
-        if len(line_list) == 2 and shared_object_name not in ['statically']:
-            shared_objects[shared_object_name] = shared_object_name
-        if shared_object_name in shared_objects:
-            ldd_of_shared_object(
-                    shared_objects,
-                    shared_objects[shared_object_name]
-            )
+def _ldd_of_shared_object(shared_objects, executable):
+    std_out = _run_bash_command(["ldd", str(executable).strip()])
+    return _std_out_to_shared_objects(shared_objects, std_out)
+
+
+def _std_out_to_shared_objects(shared_objects, std_out):
+    for std_out_line in _convert_std_out_to_list(std_out):
+        _find_dependencies(shared_objects, std_out_line)
     return shared_objects
 
 
-def convert_std_out_to_list(std_out):
+def _convert_std_out_to_list(std_out):
     return std_out.replace("\t", "").strip().split("\n")
 
 
-def main(directory):
-    find_command = ['find', directory, '-type', 'f', '-perm', '/a+x', '-exec', 'ldd', '{}', ';']
-    jvm_find_std_out = run_bash_command(find_command)
-    shared_objects = std_out_to_shared_objects({}, jvm_find_std_out)
+def _find_dependencies(shared_objects, std_out_line):
+    shared_object_details = std_out_line.split(" ")
+    shared_object_name = shared_object_details[0].strip()
+    shared_object_location = shared_object_details[2] if len(shared_object_details) > 3 else shared_object_name
+    if shared_object_name not in shared_objects.keys() and shared_object_location not in ['', 'not', 'statically']:
+        shared_objects[shared_object_name] = shared_object_location
+        _ldd_of_shared_object(
+                shared_objects,
+                shared_objects[shared_object_name]
+        )
     return shared_objects
 
 
-def run_bash_command(command):
+def _run_bash_command(command):
     popen = subprocess.Popen(command, stdout=subprocess.PIPE)
     std_out, std_err = popen.communicate("")
-    write_file(command, std_out)
     return std_out
-
-
-def write_file(command, std_out):
-    with open("tests/fixtures/{0}.txt".format("_".join(command).replace("/", "_")), 'w') as f:
-        f.write(std_out)
 
 
 if __name__ == "__main__":
