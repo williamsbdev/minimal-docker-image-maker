b92684d252e92a75115ce8617a15c107b5a34b09
Brandon Williams <williamsbdev@gmail.com>
2017-03-03 20:05:55 -0600

    [TEST] refactor test to get it passing on Travis CI

--- tests/create_minimal_image_test.py
+++ tests/create_minimal_image_test.py
@@ -3,14 +3,14 @@ from unittest import TestCase
 import create_minimal_image
 from create_minimal_image import main
 
-POPEN_COMMAND_LIST = []
+POPEN_COMMAND_LIST = ""
 
 
 class CreateMinimalImageTest(TestCase):
 
     def setUp(self):
         global POPEN_COMMAND_LIST
-        POPEN_COMMAND_LIST = []
+        POPEN_COMMAND_LIST = ""
         create_minimal_image._run_popen_command = stubbed_run_popen_command
 
     def test_main_will_correctly_return_shared_objects_and_locations(self):
@@ -20,7 +20,8 @@ class CreateMinimalImageTest(TestCase):
 
 
 def stubbed_run_popen_command(command):
-    POPEN_COMMAND_LIST.append(" ".join(command))
+    global POPEN_COMMAND_LIST
+    POPEN_COMMAND_LIST += " ".join(command) + "\n"
     try:
         with open("tests/fixtures/{0}.txt".format("_".join(command).replace("/", "_")), "r") as f:
             std_out = f.read()
@@ -31,5 +32,5 @@ def stubbed_run_popen_command(command):
 
 def get_expected_popen_comands():
     with open("tests/fixtures/expected_popen_commands.txt", "r") as f:
-        expected_popen_commands = f.read().split("\n")
-    return [command for command in expected_popen_commands if command != ""]
+        expected_popen_commands = f.read()
+    return expected_popen_commands
